d6eb625e8c5af3cd917d4684c9ee93356ec51be6
guimendes92 <gmendes92@gmail.com>
2015-04-14 14:24:18 -0300

    moving to activity

--- app/src/main/java/com/sourcecodedomain/material/activity/MainActivity.java
+++ app/src/main/java/com/sourcecodedomain/material/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sourcecodedomain.material;
+package com.sourcecodedomain.material.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.sourcecodedomain.material.R;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -20,22 +21,16 @@ public class MainActivity extends ActionBarActivity {
         setSupportActionBar(mToolbar);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
